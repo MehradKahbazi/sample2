27ea2197846f31f6b0f29339e81b9e511ce8fd99
Site14Teacher <Mehradhetfield@gmail.com>
2024-10-24 12:11:35 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// const numbers = [12, 17, 15, 12, 19];
+
+// const oddNumbers = numbers.filter(item => item %2 !== 0)
+
+
+
+// console.log(oddNumbers);
+
+const staff = [
+    { sName: 'Ali', scores: [13, 17, 18, 19] },
+    { sName: 'Reza', scores: [12, 8, 12, 17] },
+    { sName: 'Amin', scores: [11, 10, 15, 11, 19, 18] },
+    { sName: 'Nima', scores: [18, 13, 15, 12, 12] },
+]
+
+staff.map(item => {
+    const avg = item.scores.reduce((acc, score) => acc + score)/item.scores.length;
+    console.log(item.sName, ':', avg);
+})
